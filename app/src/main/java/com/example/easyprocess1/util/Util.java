cb623d9bd9a7018a2f33fd46992649f78ffbccd4
lucero <arlvillanueva13@gmail.com>
2020-03-26 20:56:51 -0600

    calculohoras

--- app/src/main/java/com/example/easyprocess1/util/Util.java
+++ app/src/main/java/com/example/easyprocess1/util/Util.java
@@ -39,8 +39,29 @@ public class Util {
         return listaCompleta;
     }
 
+
+
     /*Método ocupado para probar la clase*/
     public static void main(String[] args) {
-        
+        Util prueba = new Util();
+        String obj = prueba.Calculohoras("7:00","3:20");
+        System.out.println("valor calculado:"+obj);
+    }
+    public String Calculohoras (String Hinicio ,String Hfinal){
+        int hi, hf , mi, mf;
+        hi = Integer.parseInt(Hinicio.split(":")[0]);
+        mi = Integer.parseInt(Hinicio.split(":")[1]);
+        hf = Integer.parseInt(Hfinal.split(":")[0]);
+        mf = Integer.parseInt(Hfinal.split(":")[1]);
+        int Hrango = (12-hi)+hf;
+        int mrango = mi+mf;
+        if (mrango >59){
+            Hrango = Hrango+1;
+            mrango = mrango-60;
+        }
+        if (Hrango >12){
+            Hrango = Hrango-12;
+        }
+     return  ""+Hrango + ":"+ mrango;
     }
 }
